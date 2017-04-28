a21884b1fa5a28f32064fa8ea8f7ba6246de9896
Sven <johanjonsson5@gmail.com>
2017-04-28 15:51:19 +0200

    Updated Range

--- SensorFiles/Range_Sensor/Range_Sensor.ino
+++ SensorFiles/Range_Sensor/Range_Sensor.ino
@@ -1,40 +1,65 @@
+#include <mcp_can.h>
+#include <SPI.h>
 
+#define CAN0_INT 2    // Set INT to pin 2
+MCP_CAN CAN0(10);     // Set CS to pin 
 
-const int leftTrigPin = 5;
-const int leftEchoPin = 6;
-const int rightTrigPin = 8;
-const int rightEchoPin = 9;
+const int backSideTrigPin = 5;
+const int backSideEchoPin = 6;
+const int frontSideTrigPin = 8;
+const int frontSideEchoPin = 9;
 const int aSize = 5;
 
-int frontLeft;
-int frontRight;
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+int backSide;
+int frontSide;
+
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+
 
 void setup() {
   
-  Serial.begin(9600);
-  pinMode(leftTrigPin, OUTPUT);
-  pinMode(leftEchoPin, INPUT);
-  pinMode(rightTrigPin, OUTPUT);
-  pinMode(rightEchoPin, INPUT);
+  Serial.begin(115200);
+  pinMode(backSideTrigPin, OUTPUT);
+  pinMode(backSideEchoPin, INPUT);
+  pinMode(frontSideTrigPin, OUTPUT);
+  pinMode(frontSideEchoPin, INPUT);
 
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) 
+    Serial.println("MCP2515 Initialized Successfully!");
+  else 
+    Serial.println("Error Initializing MCP2515...");
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+
+  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  
+  Serial.println("MCP2515 Library Receive Example...");
 }
 
 void loop() {
 
-  int frontLeftArray[aSize];
-  int frontRightArray[aSize];
+  int backSideArray[aSize];
+  int frontSideArray[aSize];
 
   for(int i=0; i<aSize; i++){
     
-    frontLeftArray[i]=measure(leftTrigPin,leftEchoPin);
-    frontRightArray[i]=measure(rightTrigPin,rightEchoPin);
+    backSideArray[i]=measure(backSideTrigPin,backSideEchoPin);
+    frontSideArray[i]=measure(frontSideTrigPin,frontSideEchoPin);
   }
-  frontLeft = sortArray(frontLeftArray);
-  frontRight = sortArray(frontRightArray);
+  backSide = sortArray(backSideArray);
+  frontSide = sortArray(frontSideArray);
 
-  Serial.print(frontLeft); 
-  Serial.print(" ; ");
-  Serial.println(frontRight);
+  intTobyte(backSide);
+  sendCan();
+  intTobyte(frontSide);
+  sendCan();
+  
 }
 
 int measure(int trigPin, int echoPin){
@@ -82,5 +107,51 @@ int sortArray (int a[]){
     }
   }
   return a[2];
+}
+
+void sendCan(){
+
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   // send data per 100ms
+}
+
+void intTobyte(int value){
+
+
+  for(int i=0; i<8; i++){
+    data[i] = value%16 , HEX;
+    value = value/16;
+  Serial.println(data[i]);
+  }
+}
+
+void readCan(){
+    
+  CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+  
+  if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+    sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+  else
+    sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+
+  Serial.print(msgString);
+
+  if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+    sprintf(msgString, " REMOTE REQUEST FRAME");
+    Serial.print(msgString);
+  } else {
+    for(byte i = 0; i<len; i++){
+      sprintf(msgString, " 0x%.2X", rxBuf[i]);
+      Serial.print(msgString);
+    }
+  }
+      
+  Serial.println();
 }
 
