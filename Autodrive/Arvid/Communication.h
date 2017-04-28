c2f4a1c50e69c8c640954c462892541ad81ffbd2
ArvidEnglund <ArvidEnglund89@gmail.com>
2017-04-28 15:20:40 +0200

    ...

--- Autodrive/Arvid/Communication.h
+++ Autodrive/Arvid/Communication.h
@@ -12,20 +12,33 @@
 using namespace std;
 
 
-class Command{
-	//TODO
-};
-
 
 class Sensors {
 	//TODO
 };
 
 
-void sendCAN(){
+
+void startCAN(){
+	system("sudo ip link set can0 up type can bitrate 500000");
+}
+
+void sendCAN(int id, char msg1, int msg2){
+
+	string str = "cansend can0" + id + "#0"+ msg1 + int_to_hex(msg2);
+	cout << "str size: " << str.size() << endl;
+
+	cout << str << endl;
+	//int strS = str.size()+1;
+	char cmd[43]; //preferably size of str +1: strS
+
+	strcpy(cmd, str.c_str());
+
+
+	system(cmd);// Cmd linux command setSpeed 100
+
 
 };
-
 
 void requestUpdate(Sensors a) { //Needs to request an update first (it takes some time to go through)
 	//TODO
@@ -55,5 +68,16 @@ void update(Sensors a) { //update the values of Sensors
 		default: //Optional
 			//return fail?
 	}
+}
+
+
+template< typename T >
+std::string int_to_hex(T i)
+{
+	std::stringstream stream;
+	stream << ""
+//	<< std::setfill('0') << std::setw(sizeof(T) * 2)
+	<< std::hex << i;
+	return stream.str();
 }
 
