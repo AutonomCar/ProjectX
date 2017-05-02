e85588899992add347384e7b2ed26039bed76def
Sven <johanjonsson5@gmail.com>
2017-05-02 16:45:10 +0200

    Updated front file

--- SensorFiles/FrontArduino/FrontArduino.ino
+++ SensorFiles/FrontArduino/FrontArduino.ino
@@ -1,23 +1,42 @@
+//*************************INITIATE CAN******************************
+#include <mcp_can.h>
+#include <SPI.h>
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+
+#define CAN0_INT 2                              // Set INT to pin 2
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+//****************************************************************
+//************************INITIATE MPU9250***************************
 #include <MPU9250.h>
 #include <quaternionFilters.h>
-
 
 #define AHRS false     // Set to false for basic data read
 #define SerialDebug true  // Set to true to get Serial output for debugging
 
-const int intPin = 12; //Can be changed to 2 and 3 as external interrupts
+const int intPinI2C = 12; //Can be changed to 2 and 3 as external interrupts
+const int intPinCan = 2;
 
 MPU9250 myIMU;
-
+//***************************************************************
 void setup() {
- 
+//************************SETUP MPU9259************************
   Wire.begin();
   // TWBR = 12;  // 400 kbit/sec I2C speed
-  Serial.begin(38400);
+  Serial.begin(115200);
 
   // Set up the interrupt pin, its set as active high, push-pull
-  pinMode(intPin, INPUT);
-  digitalWrite(intPin, LOW);
+  pinMode(intPinI2C, INPUT);
+  digitalWrite(intPinI2C, LOW);
+
+  pinMode(intPinCan, INPUT);
+  digitalWrite(intPinCan, LOW);
+  
+  //***********TO DO FIX INTTERRUPT*******************
+  //attachInterrupt(0, readCan, CHANGE);
 
 
   // Read the WHO_AM_I register, this is a good test of communication
@@ -75,6 +94,21 @@ void setup() {
     }
 
   }
+//***************************************************************
+//***************************SETUP CAN***************************
+  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK)
+    Serial.println("MCP2515 Initialized Successfully!");
+  else
+    Serial.println("Error Initializing MCP2515...");
+  
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+
+  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  
+  Serial.println("MCP2515 Library Receive Example...");
+//***************************************************************
+  
 }
 
 void loop() {
@@ -87,6 +121,49 @@ void loop() {
     
   }
 
+}
+
+void sendCan(){
+  
+  byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   // send data per 100ms
+  
+}
+
+void readCan (){
+
+  byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+  
+  if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
+  {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    
+    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+      sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
+    else
+      sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
+  
+    Serial.print(msgString);
+  
+    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+      sprintf(msgString, " REMOTE REQUEST FRAME");
+      Serial.print(msgString);
+    } else {
+      for(byte i = 0; i<len; i++){
+        sprintf(msgString, " 0x%.2X", rxBuf[i]);
+        Serial.print(msgString);
+      }
+    }
+        
+    Serial.println();
+  }
 }
 
 void updateData (){
