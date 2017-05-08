9d12e4b88841e838d1ed245cadbbfc0c7c77ffeb
Arvid Englund <ArvidEnglund89@gmail.com>
2017-05-08 13:31:24 +0200

    update

--- Autodrive/Arvid/Communication.h
+++ Autodrive/Arvid/Communication.h
@@ -6,7 +6,20 @@
 * "Autonoma Elbilar"
 */
 
-#include <string>
+
+#include <string>       //String handling
+#include <cstring>      // -||-
+#include <stdio.h>      // standard input / output functions
+#include <stdlib.h>
+#include <sstream>
+
+
+//Bluetooth:
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+
 
 
 using namespace std;
@@ -14,46 +27,145 @@ using namespace std;
 
 class Command{
 	//TODO
+	//Used for imageprocessing to work correctly.
 };
 
 
-class Sensors {
-	//TODO
-};
+
+int connectBT(){
+
+	// Open Port:
+
+	int bluetooth = open( "/dev/rfcomm0", O_RDWR| O_NOCTTY );
 
 
-void sendCAN(){
 
-};
+	// Set Parameters:
 
+	struct termios tty;
+	struct termios tty_old;
+	memset (&tty, 0, sizeof tty);
 
-void requestUpdate(Sensors a) { //Needs to request an update first (it takes some time to go through)
-	//TODO
-}
-
-
-void update(Sensors a) { //update the values of Sensors
-	
-	int str[] = { 1,2,3,4,5,6 }; //get the msg //TODO
-
-	int adress;
-
-	adress += str[0] * 100;
-	adress += str[1] * 10;
-	adress += str[2] * 1;
-
-	switch (adress) {//TODO
-
-		case 111:
-			//update the sensor value
-			break; //optional
-
-		case 112:
-			//update sensor
-			break; //optional
-
-		default: //Optional
-			//return fail?
+	/* Error Handling */
+	if ( tcgetattr ( bluetooth, &tty ) != 0 ) {
+	   std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
 	}
+
+	/* Save old tty parameters */
+	tty_old = tty;
+
+	/* Set Baud Rate */
+	cfsetospeed (&tty, (speed_t)B9600);
+	cfsetispeed (&tty, (speed_t)B9600);
+
+	/* Setting other Port Stuff */
+	tty.c_cflag     &=  ~PARENB;            // Make 8n1
+	tty.c_cflag     &=  ~CSTOPB;
+	tty.c_cflag     &=  ~CSIZE;
+	tty.c_cflag     |=  CS8;
+
+	tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+	tty.c_cc[VMIN]   =  1;                  // read doesn't block
+	tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+	tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+	/* Make raw */
+	cfmakeraw(&tty);
+
+	/* Flush Port, then applies attributes */
+	tcflush( bluetooth, TCIFLUSH );
+	if ( tcsetattr ( bluetooth, TCSANOW, &tty ) != 0) {
+	   std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+	}
+
+	return bluetooth; // Return bluetooth to be able to send it to writeBL and readBL
 }
+
+void writeBT(int bluetooth, char msg[1000]){
+	char cmd[7] = "INIT \r";
+	int n_written = 0,
+	    spot = 0;
+
+	do {
+	    n_written = write( bluetooth, &cmd[spot], 1 );
+	    spot += n_written;
+	} while (cmd[spot-1] != '\r' && n_written > 0);
+}
+
+string readBT(int bluetooth){
+	int n = 0,
+	    spot = 0;
+	char buf = '\0';
+
+	/* Whole response*/
+	char response[1024];
+	memset(response, '\0', sizeof response);
+
+	do {
+	    n = read( bluetooth, &buf, 1 );
+	    sprintf( &response[spot], "%c", buf );
+	    spot += n;
+	} while( buf != '\r' && n > 0);
+
+	if (n < 0) {
+	    std::cout << "Error reading: " << strerror(errno) << std::endl;
+	}
+	else if (n == 0) {
+	    std::cout << "Read nothing!" << std::endl;
+	}
+	else {
+	    std::cout << "Response: " << response << std::endl;
+	}
+
+
+	return response; //Added return data
+}
+
+
+
+// CONTROLS
+
+void startCAN(){ // Initialize CAN-bus
+	system("sudo ip link set can0 up type can bitrate 500000");
+}
+
+
+// SPEED CONTROL
+
+void speed(int dir){
+
+    switch(dir) {
+      case 1 : //forward
+         system("cansend can0 140#01C8");
+         break;
+      case 0 : //back
+         system("cansend can0 140#00C8");
+         break;
+      case 2 : //stop
+         system("cansend can0 140#0200");
+         break;
+      default :
+        cout << "Invalid Direction (setSpeed)" << endl;
+   }
+
+}
+
+// TURNING
+
+void turn(int dir){
+    switch(dir) {
+        case 1 : //left
+            system("cansend can0 150#0100");
+            break;
+        case 2 : //right
+            system("cansend can0 150#0000");
+            break;
+      case 0 : //straight
+            system("cansend can0 150#025A");
+            break;
+      default :
+        cout << "Invalid Direction (turn)" << endl;
+   }
+}
+
 
